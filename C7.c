9b6c36d5102dd6f99d4d47d533123c803ff31c22
HarshadWaman <156565915+HarshadWaman@users.noreply.github.com>
2024-07-30 19:50:26 +0530

    C7.c

---
+++ C7.c
@@ -0,0 +1,47 @@
+
+#include<stdio.h>
+void insertion_sort(int a[],int n)
+{
+    int j,i,key,count;
+    for(j=1;j<n;j++)
+    {
+        count=0;
+        key=a[j];
+        for(i=j-1;i>=0;i--)
+        {
+            count++;
+            if(a[i]>key)
+            {
+                a[i+1]=a[i];
+            }
+            else
+              break;
+        }
+        a[i+1]=key;
+            printf("Count is:%d",count);
+    }
+}
+int main()
+{
+    int a[20],i,n;
+    
+  printf("\nEnter number of elements:");
+    scanf("%d",&n);
+    
+  printf("\nEnter array elements:");
+    for(i=0;i,n;i++)
+         a[i]=rand()%100;
+         printf("\nUnsorteg array is..\n");
+         for(i=0;i<n;i++)
+         printf("%d\t",a[i]);
+        
+  insertion_sort(a,n);
+         
+  printf("\nSorted array is..\n");
+         for(i=0;i<n;i++)
+         printf("%d\t",a[i]);
+
+     return 0;    
+}
+
+
